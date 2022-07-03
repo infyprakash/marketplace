a44a89f54413e2efcd7a53f6bd8bad99e9a56fb2
infyprakash <infymee@gmail.com>
2022-07-04 01:00:48 +0545

    eb config

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-@5isnv5dqm$nugv-o=jkr$u7-ldrw)w7^cyfa1+pl)d=i75f!b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.37.157.83']
+ALLOWED_HOSTS = ['marketplace-dev.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
@@ -130,23 +130,14 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecomdb',
-        'USER': 'admin',
-        'PASSWORD': 'InfograPHY',
-        'HOST': '52.37.157.83',
-        'PORT': '5432',
-   }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
 
 
 
