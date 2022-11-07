0e24a2ab33d49687ca842d1b6882aa849c204516
infyprakash <infymee@gmail.com>
2022-11-07 20:48:34 +0545

    Update settings.py

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -285,6 +285,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "infymee@gmail.com"
-EMAIL_HOST_PASSWORD = "zmprpevzothmuciz"
+EMAIL_HOST_PASSWORD = ""
 
 # ACCOUNT_EMAIL_VERIFICATION = "mandatory"
