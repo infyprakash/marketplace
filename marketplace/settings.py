2ef647e1e0a6fc7a6b47c92f2065e59f29dcfc8b
infyprakash <infymee@gmail.com>
2022-11-07 20:55:13 +0545

    Update settings.py

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['marketplace-dev.us-west-2.elasticbeanstalk.com','127.0.0.1','connectopasal.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
